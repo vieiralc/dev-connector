e2c432e451dbaf67f397b2ae5b5fc2f86949f3f9
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-07-09 09:47:51 -0300

    add path to router

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require("express")
+const mongoose = require("mongoose")
 
-const app = express();
+const users = require("./routes/api/users")
+const profile = require("./routes/api/profile")
+const posts = require("./routes/api/posts")
+
+const app = express()
 
 // DB config
-const db = require("./config/keys").mongoURI;
+const db = require("./config/keys").mongoURI
 
 // Connect to MongoDB
 mongoose
@@ -13,9 +17,14 @@ mongoose
     { useNewUrlParser: true }
   )
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err))
 
-app.get("/", (req, res) => res.send("hello world"));
+app.get("/", (req, res) => res.send("hello world"))
+
+// Use Routes
+app.use("/api/users", users)
+app.use("/api/profile", profile)
+app.use("/api/posts", posts)
 
 const port = process.env.PORT || 5000;
 
