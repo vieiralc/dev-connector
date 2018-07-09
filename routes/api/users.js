f98f794e2949305d71e767d40a318a38c6dfc57c
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-07-09 10:47:03 -0300

    finish register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,50 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+
+// Load User modal
+const User = require('../../models/User')
 
 // @router  GET api/users/test
 // @dsc     Testes users route
 // @access  Public 
 router.get('/test', (req, res) => res.json({msg: "Users Works"}))
+
+// @router  POST api/users/register
+// @dsc     Register user
+// @access  Public 
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) 
+                return res.status(400).json({email: 'Email already exists'})
+            else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // size
+                    r: 'pg',  // rating
+                    d: 'mm'   // default 
+                })
+
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err
+                        newUser.password = hash
+                        newUser
+                            .save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err))
+                    })
+                })
+            }
+        })
+})
 
 module.exports = router
