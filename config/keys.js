c55846495c3f8f3ae8044d6cc23e4e3d6f122e52
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-07-10 08:25:32 -0300

    add secret key

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://vieiralc:l12345@ds143778.mlab.com:43778/my_devconnector"
+  mongoURI: "mongodb://vieiralc:l12345@ds143778.mlab.com:43778/my_devconnector",
+  secretOrKey: 'secret'
 };
