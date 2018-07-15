1e2f08b414c8f4ba691a734e4fbe52cfc81b919b
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-07-15 16:35:58 -0300

    add route to fetch all users

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -69,6 +69,24 @@ router.get('/user/:user_id', (req, res) => {
         .catch(err => res.status(404).json({profile: 'There is no profile for this user'}))
 })
 
+// @router  GET api/profile/all
+// @dsc     Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+    const errors = {}
+
+    Profile.find()
+    .populate('user', ['name','avatar'])
+    .then(profiles => {
+        if (!profiles) {
+            errors.noprofile = 'There are no profiles'
+            res.status(404).json(errors)
+        }
+
+        res.json(profiles)
+    })
+    .catch(err => res.status(404).json({profile: 'There are no profiles'}))
+})
 
 // @router  POST api/profile/
 // @dsc     Create or edit user profile
