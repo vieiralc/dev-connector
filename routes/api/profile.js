a0d876c5743a1c1e7f99f6f7dc6fa712a9b5742b
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-07-15 17:40:16 -0300

    add delete exp/edu routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -147,7 +147,7 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
         })
 })
 
-// @router  POST api/profile/experiences
+// @router  POST api/profile/experience
 // @dsc     Add experience to profile
 // @access  Private
 router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -205,6 +205,48 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
         profile.save().then(profile => res.json(profile))
     })
     .catch(err => res.status(404).json(err))
+})
+
+// @router  DELETE api/profile/experience/:exp_id
+// @dsc     Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    
+    Profile.findOne({ user: req.user.id }).then(profile => {
+        // Get remove index
+        const removeIndex = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id)
+
+        // Splice out of array
+        profile.experience.splice(removeIndex, 1)
+
+        // Save
+        profile.save().then(profile => res.json(profile))
+    })
+    .catch(err => res.status(404).json(err))
+
+})
+
+// @router  DELETE api/profile/education/:edu_id
+// @dsc     Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    
+    Profile.findOne({ user: req.user.id }).then(profile => {
+        // Get remove index
+        const removeIndex = profile.education
+            .map(item => item.id)
+            .indexOf(req.params.edu_id)
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1)
+
+        // Save
+        profile.save().then(profile => res.json(profile))
+    })
+    .catch(err => res.status(404).json(err))
+
 })
 
 module.exports = router
