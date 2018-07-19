2b4d6e6df3aef2b0a0f9d36bdac5aa3b80cba068
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-07-19 02:55:28 -0300

    format import lines

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
-import Landing from "./components/layout/Landing";
+import Navbar   from "./components/layout/Navbar";
+import Footer   from "./components/layout/Footer";
+import Landing  from "./components/layout/Landing";
 import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
+import Login    from "./components/auth/Login";
 
 import "./App.css";
 
 class App extends Component {
+  
   render() {
     return (
       <Router>
