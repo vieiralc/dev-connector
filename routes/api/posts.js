63ab8efb6681cbcdebe4748cacf5ac864093e6f2
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-07-21 12:04:19 -0300

    Delete unnecessary promise

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -55,21 +55,18 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 // @desc   Delete post
 // @access Private
 router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-    
-    Profile.findOne({user: req.user.id})
-        .then(profile => {
-            Post.findById(req.params.id)
-                .then(post => {
-                    // Check for post owner
-                    if (post.user.toString() !== req.user.id) {
-                        return res.status(401).json({ notauthorized: 'User not authorized' })
-                    }
+ 
+    Post.findById(req.params.id)
+        .then(post => {
+            // Check for post owner
+            if (post.user.toString() !== req.user.id) {
+                return res.status(401).json({ notauthorized: 'User not authorized' })
+            }
 
-                    // Delete
-                    post.remove().then(() => res.json({ success: true }))
-                })
-                .catch(err => res.status(404).json({ postnotfound: 'Post not found' }))
+            // Delete
+            post.remove().then(() => res.json({ success: true }))
         })
+        .catch(err => res.status(404).json({ postnotfound: 'Post not found' }))  
 })
 
 // @route  POST api/posts/like/:id
