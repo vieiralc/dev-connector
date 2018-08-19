361d2cdad5a848309f43d69fd39cb3d9f980c688
Lucas <vieira.lucasmm@gmail.com>
2018-08-19 20:16:41 -0300

    check for expired token

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
-import { setCurrentUser } from './actions/authActions'
+import { setCurrentUser, logoutUser } from './actions/authActions'
 
 import Navbar   from "./components/layout/Navbar"
 import Footer   from "./components/layout/Footer"
@@ -24,6 +24,17 @@ if (localStorage.jwtToken) {
 
   // set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded))
+
+  // check for expired token
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser)
+
+    // TODO: Clear current profile
+    // Redirect to login
+    window.location.href = '/login'
+  }
 }
 
 class App extends Component {
