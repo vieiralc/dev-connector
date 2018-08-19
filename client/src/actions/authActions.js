b22267215fdd617510842727d862bc1986621301
Lucas <vieira.lucasmm@gmail.com>
2018-08-19 19:51:18 -0300

    logoutUser action done

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -53,3 +53,15 @@ export const setCurrentUser = decoded => {
         payload: decoded
     }
 }
+
+// Log user out
+export const logoutUser = () => dispatch => {
+    // Remove token from localStorage
+    localStorage.removeItem('jwtToken')
+
+    // Remove auth header for future req
+    setAuthToken(false)
+
+    // Set current user to {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}))
+}
