46e9783864a19f0dbb216d86a38b4e3e40587bf4
Lucas <vieira.lucasmm@gmail.com>
2018-08-24 21:45:13 -0300

    add route to dashboard + clearCurrentProfile

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import store from './store'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
 import { setCurrentUser, logoutUser } from './actions/authActions'
+import { clearCurrentProfile } from "./actions/profileActions"
 
-import Navbar   from "./components/layout/Navbar"
-import Footer   from "./components/layout/Footer"
-import Landing  from "./components/layout/Landing"
-import Register from "./components/auth/Register"
-import Login    from "./components/auth/Login"
+import Navbar    from "./components/layout/Navbar"
+import Footer    from "./components/layout/Footer"
+import Landing   from "./components/layout/Landing"
+import Register  from "./components/auth/Register"
+import Login     from "./components/auth/Login"
+import Dashboard from "./components/dashboard/Dashboard"
 
 import "./App.css";
 
@@ -29,9 +31,10 @@ if (localStorage.jwtToken) {
   const currentTime = Date.now() / 1000
   if (decoded.exp < currentTime) {
     // Logout user
-    store.dispatch(logoutUser)
+    store.dispatch(logoutUser())
 
-    // TODO: Clear current profile
+    // Clear current profile
+    store.dispatch(clearCurrentProfile())
     // Redirect to login
     window.location.href = '/login'
   }
@@ -49,6 +52,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
