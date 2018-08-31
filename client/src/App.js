037bf3aa8be0c2265d6b8c9187bd41ffcb37c5af
Lucas <vieira.lucasmm@gmail.com>
2018-08-30 22:38:39 -0300

    add create-profile private route

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,15 @@ import setAuthToken from './utils/setAuthToken'
 import { setCurrentUser, logoutUser } from './actions/authActions'
 import { clearCurrentProfile } from "./actions/profileActions"
 
-import PrivateRoute from './components/common/PrivateRoute'
+import PrivateRoute  from './components/common/PrivateRoute'
 
-import Navbar    from "./components/layout/Navbar"
-import Footer    from "./components/layout/Footer"
-import Landing   from "./components/layout/Landing"
-import Register  from "./components/auth/Register"
-import Login     from "./components/auth/Login"
-import Dashboard from "./components/dashboard/Dashboard"
+import Navbar        from "./components/layout/Navbar"
+import Footer        from "./components/layout/Footer"
+import Landing       from "./components/layout/Landing"
+import Register      from "./components/auth/Register"
+import Login         from "./components/auth/Login"
+import Dashboard     from "./components/dashboard/Dashboard"
+import CreateProfile from "./components/create-profile/CreateProfile"
 
 import "./App.css";
 
@@ -56,6 +57,9 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch> 
             </div>
             <Footer />
