8ea4670239f90a352bb66ad0a484841fafb9ed8e
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-14 10:09:49 -0200

    add deleteAccount and createProfile Actions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types'
+import { 
+    GET_PROFILE, 
+    PROFILE_LOADING, 
+    CLEAR_CURRENT_PROFILE, 
+    GET_ERRORS, 
+    SET_CURRENT_USER 
+} from './types'
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -17,6 +23,39 @@ export const getCurrentProfile = () => dispatch => {
             dispatch({
                 type: GET_PROFILE,
                 payload: {}
+            })
+        )
+}
+
+// Delete account and profile
+export const deleteAccount = () => dispatch => {
+    if (window.confirm('Are you sure? This cannot be undone!')) {
+        axios
+            .delete('/api/profile')
+            .then(res => 
+                dispatch({
+                    type: SET_CURRENT_USER,
+                    payload: {}
+                })
+            )
+            .catch(err => 
+                dispatch({
+                    type: GET_ERRORS,
+                    payload: err.response.data
+                })
+            )
+    }
+}
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+        .post('/api/profile', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
             })
         )
 }
