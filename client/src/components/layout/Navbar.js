5bb945a49a28b3c8ef1a37f566436eafd98cc705
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-15 21:14:56 -0200

    display dashboard link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -21,7 +21,7 @@ class Navbar extends Component {
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
             <Link className="nav-link" to="/dashboard">
-              dashboard
+              Dashboard
             </Link>
           </li>
         <li className="nav-item">
