e154c57d76b4b74df0e7e0b8eefb7d8ce184d8ac
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-15 21:13:41 -0200

    addExp addEdu and delete redux actions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -27,6 +27,68 @@ export const getCurrentProfile = () => dispatch => {
         )
 }
 
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+    axios
+        .post('/api/profile/experience', expData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })    
+        )
+}
+
+// Add education
+export const addEducation = (eduData, history) => dispatch => {
+    axios
+        .post('/api/profile/education', eduData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })    
+        )
+}
+
+// delete experience
+export const deleteExperience = id => dispatch => {
+    axios
+        .delete(`/api/profile/experience/${id}`)
+        .then(res => 
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        )
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })    
+        )
+}
+
+// delete education
+export const deleteEducation = id => dispatch => {
+    axios
+        .delete(`/api/profile/education/${id}`)
+        .then(res => 
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        )
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })    
+        )
+}
+
 // Delete account and profile
 export const deleteAccount = () => dispatch => {
     if (window.confirm('Are you sure? This cannot be undone!')) {
