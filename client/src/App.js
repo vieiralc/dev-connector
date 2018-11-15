a38faeab1f8282600e6ad3d6932c38f746a3d173
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-15 21:13:13 -0200

    add routes to edu and exp

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import Login         from "./components/auth/Login"
 import Dashboard     from "./components/dashboard/Dashboard"
 import CreateProfile from "./components/create-profile/CreateProfile"
 import EditProfile   from "./components/edit-profile/EditProfile"
+import AddExperience from "./components/add-credentials/AddExperience"
+import AddEducation  from "./components/add-credentials/AddEducation"
 
 import "./App.css";
 
@@ -64,6 +66,12 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+              </Switch> 
+              <Switch>
+                <PrivateRoute exact path="/add-experience" component={AddExperience} />
+              </Switch> 
+              <Switch>
+                <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch> 
             </div>
             <Footer />
