609485723bcdf8a6c9ef28d1dbd7c874e681b3f4
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-30 20:51:30 -0200

    add route to post

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,10 +20,15 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
-              Dashboard
-            </Link>
-          </li>
+          <Link className="nav-link" to="/feed">
+            Post Feed
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
         <li className="nav-item">
           <a href="" onClick={this.onLogoutClick.bind(this)} className="nav-link">
             <img src={user.avatar} alt={user.name} title="You must have a gravatar connected to your email to display an image"
