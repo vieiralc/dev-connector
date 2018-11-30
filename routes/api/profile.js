4551bbe765261c7fc245422c63b24960975f2dae
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-30 20:04:10 -0200

    fix social media updating bug

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -103,7 +103,10 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     profileFields.user = req.user.id
     if (req.body.handle) profileFields.handle                 = req.body.handle
     if (req.body.company) profileFields.company               = req.body.company
-    if (req.body.website) profileFields.website               = req.body.website
+    if (req.body.website) 
+        profileFields.website = req.body.website
+    else 
+        profileFields.website = ""
     if (req.body.location) profileFields.location             = req.body.location
     if (req.body.bio) profileFields.bio                       = req.body.bio
     if (req.body.status) profileFields.status                 = req.body.status
@@ -120,6 +123,7 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     if (req.body.github) profileFields.social.github       = req.body.github
     if (req.body.instagram) profileFields.social.instagram = req.body.instagram
     if (req.body.linkedin) profileFields.social.linkedin   = req.body.linkedin
+    if (req.body.twitter) profileFields.social.twitter     = req.body.twitter
     if (req.body.facebook) profileFields.social.facebook   = req.body.facebook
 
     Profile.findOne({ user: req.user.id })
