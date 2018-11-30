cfc899c6b8043607fc5bdffdf073b73db273e747
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-30 20:01:09 -0200

    add get ProfileByHandle redux action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -5,7 +5,8 @@ import {
     PROFILE_LOADING, 
     CLEAR_CURRENT_PROFILE, 
     GET_ERRORS, 
-    SET_CURRENT_USER 
+    SET_CURRENT_USER, 
+    GET_PROFILES
 } from './types'
 
 // Get current profile
@@ -23,6 +24,25 @@ export const getCurrentProfile = () => dispatch => {
             dispatch({
                 type: GET_PROFILE,
                 payload: {}
+            })
+        )
+}
+
+// Get profile by handle
+export const getProfileByHandle = (handle) => dispatch => {
+    dispatch(setProfileLoading())
+    axios
+        .get(`/api/profile/handle/${handle}`)
+        .then(res => 
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        )
+        .catch(err => 
+            dispatch({
+                type: GET_PROFILE,
+                payload: null
             })
         )
 }
@@ -85,6 +105,25 @@ export const deleteEducation = id => dispatch => {
             dispatch({
                 type: GET_ERRORS,
                 payload: err.response.data
+            })    
+        )
+}
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading())
+    axios
+        .get('/api/profile/all')
+        .then(res => 
+            dispatch({
+                type: GET_PROFILES,
+                payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch({
+                type: GET_PROFILES,
+                payload: null
             })    
         )
 }
