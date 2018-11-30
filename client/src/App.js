40033107ca9fd9dccebd186f37f032a86c30ee0c
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-30 20:00:43 -0200

    add route to not find and profile

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,10 @@ import CreateProfile from "./components/create-profile/CreateProfile"
 import EditProfile   from "./components/edit-profile/EditProfile"
 import AddExperience from "./components/add-credentials/AddExperience"
 import AddEducation  from "./components/add-credentials/AddEducation"
+import Profiles      from "./components/profiles/Profiles"
+import Profile       from "./components/profile/Profile"
+
+import NotFound from "./components/not-found/NotFound"
 
 import "./App.css";
 
@@ -58,6 +62,8 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
@@ -73,6 +79,7 @@ class App extends Component {
               <Switch>
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch> 
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
