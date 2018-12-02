d8756d6266fa71634f0bf88ea61afd50b4fd0c58
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-12-02 10:37:36 -0200

    add post reducer

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST } from '../actions/types';
+import { ADD_POST, GET_POSTS, DELETE_POST, POST_LOADING, GET_POST } from '../actions/types';
 
 const initialState = {
     posts: [],
@@ -8,11 +8,35 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case POST_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        case GET_POSTS: 
+            return {
+                ...state,
+                posts: action.payload,
+                loading: false
+            }
         case ADD_POST:
             return {
                 ...state,
                 posts: [action.payload, ...state.posts]
             }
+        case DELETE_POST: {
+            return {
+                ...state,
+                posts: state.posts.filter(post => post._id !== action.payload)
+            }
+        }
+        case GET_POST: {
+            return {
+                ...state,
+                post: action.payload,
+                loading: false
+            }
+        }
         default:
             return state;
     }
