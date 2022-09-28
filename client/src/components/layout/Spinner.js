45c0f6a77e12a656dae3242af5f8196821e7f50f
Lucas Vieira <vieira.lucasmm@gmail.com>
2022-09-28 20:57:20 -0300

    add Spinner component

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,9 +1,8 @@
-import React from 'react'
 import spinner from './spinner.gif'
 
 export default () => {
   return (
-    <div>
+    <div className='container'>
         <img 
             src={spinner} 
             style={{ width: '200px', margin: 'auto', display: 'block' }}
