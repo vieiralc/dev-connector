e02673e56f14f6396993f0d1a456b20d56f05ec7
Lucas Vieira <vieira.lucasmm@gmail.com>
2022-09-28 20:56:59 -0300

    add clear profile reducer

--- client/src/redux/reducers/profileSlice.js
+++ client/src/redux/reducers/profileSlice.js
@@ -3,7 +3,9 @@ import { createSlice } from "@reduxjs/toolkit"
 export const profileSlice = createSlice({
     name: 'profile',
     initialState: {
-        profile: null,
+        profile: {
+            name: 'test'
+        },
         profiles: [],
         repos: [],
         loading: true,
@@ -17,10 +19,17 @@ export const profileSlice = createSlice({
         profileError: (state, { payload }) => {
             state.error = payload
             state.loading = false
+        },
+        clearProfile: (state, action) => {
+            state.profile = null,
+            state.profiles = [],
+            state.repos = [],
+            state.loading = true,
+            state.error = {}
         }
     }
 })
 
-export const { getProfile, profileError } = profileSlice.actions
+export const { getProfile, profileError, clearProfile } = profileSlice.actions
 
 export default profileSlice.reducer
