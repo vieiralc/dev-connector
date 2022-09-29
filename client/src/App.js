e04e129b37b96d9286847f713d399cc36ece1793
Lucas Vieira <vieira.lucasmm@gmail.com>
2022-09-28 21:37:58 -0300

    Create initial dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,61 @@
-import React, { useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-import Alert from './components/layout/Alert'
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
-import Register from './components/auth/Register'
-import Login from './components/auth/Login'
-import Dashboard from './components/dashboard/Dashboard'
-import PrivateRoute from './components/routing/PrivateRoute'
+import Alert from './components/layout/Alert';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
-import setAuthToken from './utils/setAuthToken'
-import { loadUser } from './redux/actions/auth/loadUser'
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './redux/actions/auth/loadUser';
 
-import './App.css'
+import './App.css';
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token)
+  setAuthToken(localStorage.token);
 }
 
 const App = () => {
-
   useEffect(() => {
-    store.dispatch(loadUser())
-  }, [])
+    store.dispatch(loadUser());
+  }, []);
 
   return (
     <Provider store={store}>
-      <Navbar/>
-      <Alert/>
+      <Navbar />
+      <Alert />
       <Routes>
-        <Route exact path='/' element={<Landing />}/>
-        <Route exact path='/register' element={<Register />}/>
-        <Route exact path='/login' element={<Login />}/>
-        <Route exact path='/dashboard' element={
-          <PrivateRoute>
-            <Dashboard/>
-          </PrivateRoute>
-        }/>
+        <Route exact path='/' element={<Landing />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route
+          exact
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path='/create-profile'
+          element={
+            <PrivateRoute>
+              <CreateProfile />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,103 +1,39 @@
 import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { getCurrentProfile } from "../../redux/actions/profile/getCurrentProfile"
+import Spinner from '../layout/Spinner'
 
 const Dashboard = () => {
 
   const dispatch = useDispatch()
+  const loading = useSelector(state => state.profile.loading)
+  const profile = useSelector(state => state.profile.profile)
+  const user = useSelector(state => state.auth.user)
 
   useEffect(() => {
     dispatch(getCurrentProfile())
   }, [])
 
-  return (
-    <div className='container'>Dashboard</div>
+  return loading && profile === null ? <Spinner/> : (
+    <div className="container">
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>
+        Welcome { user && user.name }
+      </p>
+      {
+        profile !== null ? (
+          <>
+            has profile
+          </>
+        ) : (
+          <>
+            does not have a profile
+          </>
+        )
+      }
+    </div>
   )
 }
 
 export default Dashboard
-
-// import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
-// import PropTypes from 'prop-types'
-// import { connect } from 'react-redux'
-// import { getCurrentProfile, deleteAccount } from '../../actions/profileActions'
-// import Spinner from '../common/Spinner'
-// import ProfileACtions from './ProfileActions'
-// import Experience from './Experience'
-// import Education from './Education'
-
-// class Dashboard extends Component {
-    
-//     componentDidMount() {
-//         this.props.getCurrentProfile()
-//     }
-
-//     onDeleteClick(e) {
-//         this.props.deleteAccount()   
-//     }
-
-//     render() {
-
-//         const { user } = this.props.auth
-//         const { profile, loading } = this.props.profile
-
-//         let dashBoardContent
-
-//         if (profile  === null || loading) {
-//             dashBoardContent = <Spinner/>
-//         } else {
-//             // Check if logged in user has profile data
-//             if (Object.keys(profile).length > 0) {
-//                 dashBoardContent = (
-//                     <div>
-//                         <p className="lead text-muted">Welcome <Link to={`/profile/${profile.handle}`}> {user.name} </Link> </p>
-//                         <ProfileACtions/>
-//                         <Experience experience={profile.experience} />
-//                         <Education education={profile.education} />
-//                         <div style={{marginBottom: '60px'}}/>
-//                         <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Accounts</button>
-//                     </div>
-//                 )
-//             } else {
-//                 // User is logged in but has no profile
-//                 dashBoardContent = (
-//                     <div>
-//                         <p className="lead text-muted">Welcome { user.name } </p>
-//                         <p> You have not yet setup a profile, please add some info</p>
-//                         <Link to="/create-profile" className="btn btn-lg btn-info">
-//                             Create Profile
-//                         </Link>
-//                     </div>
-//                 )
-//             }
-//         }
-
-//         return (
-//             <div className="dashboard">
-//                 <div className="container">
-//                     <div className="row">
-//                         <div className="col-md-12">
-//                             <h1 className="display-4">Dashboard</h1>
-//                             {dashBoardContent}
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-// Dashboard.propTypes = {
-//     getCurrentProfile: PropTypes.func.isRequired,
-//     deleteAccount: PropTypes.func.isRequired,
-//     auth: PropTypes.object.isRequired,
-//     profile: PropTypes.object.isRequired
-// }
-
-// const mapStateToProps = state => ({
-//     profile: state.profile,
-//     auth: state.auth
-// })
-
-// export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard)
