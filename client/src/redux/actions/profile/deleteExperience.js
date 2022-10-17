4f6a35e36ab8559a1e16f8798a9857bace2adee2
Lucas Vieira <vieira.lucasmm@gmail.com>
2022-10-17 17:32:28 -0300

    Add delete Education, Experience and Account actions

---
+++ client/src/redux/actions/profile/deleteExperience.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { defaultHeaders } from '../../../utils/defaultHeaders';
+import { setAlert } from '../../reducers/alertSlice';
+import { updateProfile } from '../../reducers/profileSlice';
+
+export function deleteExperience(experience_id) {
+  return async function deleteExperience(dispatch, getState) {
+    try {
+        const response = await axios.delete(
+            `api/profile/experience/${experience_id}`,
+            defaultHeaders
+        );
+        dispatch(updateProfile(response.data));
+        dispatch(setAlert({ message: 'Experience Removed', alertType: 'success' }));
+    } catch (err) {
+        dispatch(
+            profileError({
+                msg: err.response.statusText,
+                status: err.response.status,
+            })
+        );
+    }
+  };
+}
